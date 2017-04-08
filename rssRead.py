76c7c33de5aa684a75b6a7d02220d9956e0f0066
苏俊玮 <sujunwei@sujunweideMacBook-Pro.local>
2017-04-08 21:45:17 +0800

    使用iocp解析rss的链接

--- rssRead.py
+++ rssRead.py
@@ -5,6 +5,8 @@ import re
 import csv
 import opml
 import MySQLdb
+import os
+import json
 
 class News:
     title = ''
@@ -23,12 +25,12 @@ def readRssList(path):
     dom = opml.parse(path)
     for outline in dom:
         print outline.xmlUrl
-        readRss(outline.xmlUrl)
+        readRss(outline.xmlUrl,outline.title)
 
 
-def readRss(url):
+def readRss(url,rsstitle):
     d = feedparser.parse(url)
-
+    newslist = []
     for e in d.entries:
         new = News()
         new.title = e['title']
@@ -44,8 +46,27 @@ def readRss(url):
         # print new.title
         # print new.content
         newslist.append(new)
-    connectDb()
+        getIocFromUrl(new.link)
+    # createhtml(rsstitle,newslist)
+    # connectDb()
     # print "--------------------------------"
+
+
+def createhtml(rsstitle,list):
+    dirname = r'./'+rsstitle
+    os.mkdir(dirname)
+    for item in list:
+        filename = item.title.encode('utf-8') + ".html"
+        filename = filename.replace('/', '')
+        print filename
+        f = open(dirname + "/" + filename, "w")
+        try:
+            f.write(item.content.encode('utf-8'))
+            f.write(item.link.encode('utf-8'))
+        except Exception, e:
+            print('\033[1;31;40m')
+            print e
+        f.close()
 
 
 def writeCsv():
@@ -61,26 +82,36 @@ def writeCsv():
             print data
     csvfile.close()
 
-def connectDb():
+def connectDb(text):
     # 打开数据库连接
-    db = MySQLdb.connect("localhost", "root", "", "apt_ioc_pro", charset="utf8")
+    db = MySQLdb.connect("localhost", "root", "root", "apt_ioc_pro", charset="utf8")
     # print text
     # 使用cursor()方法获取操作游标
     cursor = db.cursor()
     # print text['file']
     try:
-        for new in newslist:
         # 使用execute方法执行SQL语句
-            cursor.execute("INSERT INTO rss values(null,'%s','%s','%s')" % (new.title.encode('utf-8'),new.content.encode('utf-8'),new.link.encode('utf-8')))
+        cursor.execute("INSERT INTO rss_ioc values(null,'%s','%s','%s','%s')" % (text['file'], text['type'], text['match'].encode('utf-8').replace('\'',''), text['page']))
         db.commit()
-    except BaseException,Argument:
-        print Argument
-        print new.title.encode('utf-8'), new.content.encode('utf-8'), new.link.encode('utf-8')
+    except Exception,e:
+        print e
+        print "sql arg: "+text['file'], text['type'], text['match'], text['page']
         db.rollback()
 
     # 关闭数据库连接
     db.close()
 
-newslist=[]
-readRssList('/Users/sujunwei/work/Apt_Ioc_work/RSS_Bot.opml')
+def getIocFromUrl(url):
+    cmd = 'iocp ' + url + ' -p ./patterns.ini -i html -o json -l request,BeautifulSoup'
+    try:
+        tmps = os.popen(cmd).readlines()
+        for tmp in tmps:
+            text = json.loads(tmp)
+            connectDb(text)
+    except Exception,e:
+        print e
+
+
+# newslist=[]
+readRssList('./RSS_Bot.opml')
 # readRss('http://bobao.360.cn/rss?type=news')
